15a7db08ec3ac5478a87153d32e83d54b700c42b
sumedhsph <sumedh.sph@gmail.com>
2024-02-20 16:14:45 +0530

    Project setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold underline">Tailwind project</h1>
+      <button class="btn w-64 rounded-full">Button</button>
+    </div>
+  );
+}
+
+export default App;
