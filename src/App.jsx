1771fd7f35eb958f551d44a18a6e3ca028c10278
sumedhsph <sumedh.sph@gmail.com>
2024-02-21 10:34:48 +0530

    Project setup done, added tailwind and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomeLayout from "./pages/HomeLayout";
+
+
+function App() {
+  return (
+    <>
+     <HomeLayout/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function HomeLayout() {
+  return (
+    <main>
+      <Navbar/>
+    </main>
+  )
+}
+
+export default HomeLayout
